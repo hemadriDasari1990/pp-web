430dedf792aa42f4bd26f34d8a086468107996fd
Hemadri Dssari <hemadri.dasari1990@gmail.com>
2020-01-09 14:10:12 +0530

    remove unsed modules

--- src/components/Dashboard/components/Dashboard.js
+++ src/components/Dashboard/components/Dashboard.js
@@ -1,73 +1,81 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles';
-import * as userProfileActions from '../../UserProfile/actions';
-import {Map, fromJS} from 'immutable';
-import { BrowserRouter as Router, Route, Redirect, withRouter, Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
-import firebase from '../../../firebase';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import HighLightOffIcon from '@material-ui/icons/highLightOff';
-import CheckCircleOutlineIcon from '@material-ui/icons/checkCircleOutline';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import PostList from  './PostList';
-import PostsInfo from './PostsInfo';
-import RecentPosts from './RecentPosts';
-import TopPosts from './TopPosts';
-import ListIcon from '@material-ui/icons/List';
-import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck';
-import CallReceivedIcon from '@material-ui/icons/CallReceived';
-import formateNumber from '../../../util/formateNumber';
-import Loader from '../../Loader/components/Loader';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import withStyles from '@material-ui/core/styles/withStyles'
+import * as userProfileActions from '../../UserProfile/actions'
+import { Map, fromJS } from 'immutable'
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  withRouter,
+  Switch,
+} from 'react-router-dom'
+import { connect } from 'react-redux'
+import firebase from '../../../firebase'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import ListItemText from '@material-ui/core/ListItemText'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import PostList from './PostList'
+import PostsInfo from './PostsInfo'
+import RecentPosts from './RecentPosts'
+import TopPosts from './TopPosts'
+import ListIcon from '@material-ui/icons/List'
+import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck'
+import CallReceivedIcon from '@material-ui/icons/CallReceived'
+import formateNumber from '../../../util/formateNumber'
+import Loader from '../../Loader/components/Loader'
 
 class DashBoard extends Component {
-  constructor(props){
-    super(props);
+  constructor(props) {
+    super(props)
     this.state = {
       showMyPosts: false,
       user: {},
       givenPosts: [],
       receivedPosts: [],
       isGivenPost: false,
-      isReceivedPost: true
+      isReceivedPost: true,
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     new firebase.auth().onAuthStateChanged(async user => {
-      if(user && Array.isArray(user.providerData) && user.providerData.length) {
+      if (
+        user &&
+        Array.isArray(user.providerData) &&
+        user.providerData.length
+      ) {
         await this.props.getPostsByUser(user.providerData[0].uid).then(data => {
           this.setState({
             user: user.providerData[0],
-            receivedPosts: data.data
-          });
-        });
+            receivedPosts: data.data,
+          })
+        })
       }
-    });
+    })
   }
 
   updatePosts = posts => {
     this.setState({
-      receivedPosts: posts
-    });
+      receivedPosts: posts,
+    })
   }
 
   handleUser = event => {
-    this.props.history.push('/dashboard');
+    this.props.history.push('/dashboard')
   }
 
   handleGivenPosts = async event => {
@@ -75,9 +83,9 @@ class DashBoard extends Component {
       this.setState({
         givenPosts: data.data,
         isGivenPost: true,
-        isReceivedPost: false
-      });
-    });
+        isReceivedPost: false,
+      })
+    })
   }
 
   handleReceivedPosts = async event => {
@@ -85,96 +93,136 @@ class DashBoard extends Component {
       this.setState({
         receivedPosts: data.data,
         isGivenPost: false,
-        isReceivedPost: true
-      });
-    });
+        isReceivedPost: true,
+      })
+    })
   }
 
-
-  render(){
-    const { user, givenPosts, receivedPosts, isReceivedPost, isGivenPost} = this.state;
-    const { postsLoading } = this.props;
+  render() {
+    const {
+      user,
+      givenPosts,
+      receivedPosts,
+      isReceivedPost,
+      isGivenPost,
+    } = this.state
+    const { postsLoading } = this.props
     return (
       <React.Fragment>
-        
-        {postsLoading ? <Loader /> : (<div className="conatiner">
-           <div className="row">
-             <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+        {postsLoading ? (
+          <Loader />
+        ) : (
+          <div className="conatiner">
+            <div className="row">
+              <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
                 <List component="nav" aria-label="main mailbox folders">
                   <ListItem button onClick={event => this.handleUser(event)}>
                     <ListItemAvatar>
-                      <Avatar size="small" src={user.photoURL}/>
+                      <Avatar size="small" src={user.photoURL} />
                     </ListItemAvatar>
-                    <ListItemText
-                      primary={user.displayName}
-                    />
+                    <ListItemText primary={user.displayName} />
                     <ListItemSecondaryAction>
-                      <IconButton color="primary" edge="end" aria-label="delete">
+                      <IconButton
+                        color="primary"
+                        edge="end"
+                        aria-label="delete"
+                      >
                         <MoreHorizIcon />
                       </IconButton>
                     </ListItemSecondaryAction>
                   </ListItem>
-                  <ListItem button onClick={event => this.handleGivenPosts(event)}>
+                  <ListItem
+                    button
+                    onClick={event => this.handleGivenPosts(event)}
+                  >
                     <ListItemAvatar>
                       <PlaylistAddCheckIcon color="primary" />
                     </ListItemAvatar>
-                    <ListItemText
-                      primary="Given Posts"
-                    />
-                    <ListItemSecondaryAction style={{marginRight:20}}>
-                      {isGivenPost && givenPosts ? formateNumber(givenPosts.length): 0}
+                    <ListItemText primary="Given Posts" />
+                    <ListItemSecondaryAction style={{ marginRight: 20 }}>
+                      {isGivenPost && givenPosts
+                        ? formateNumber(givenPosts.length)
+                        : 0}
                     </ListItemSecondaryAction>
                   </ListItem>
-                  <ListItem button onClick={event => this.handleReceivedPosts(event)}>
+                  <ListItem
+                    button
+                    onClick={event => this.handleReceivedPosts(event)}
+                  >
                     <ListItemAvatar>
                       <CallReceivedIcon color="primary" />
                     </ListItemAvatar>
-                    <ListItemText
-                      primary="Received Posts"
-                    />
-                    <ListItemSecondaryAction style={{marginRight:20}}>
-                      {isReceivedPost && receivedPosts ? formateNumber(receivedPosts.length): 0}
+                    <ListItemText primary="Received Posts" />
+                    <ListItemSecondaryAction style={{ marginRight: 20 }}>
+                      {isReceivedPost && receivedPosts
+                        ? formateNumber(receivedPosts.length)
+                        : 0}
                     </ListItemSecondaryAction>
                   </ListItem>
                 </List>
-                {isGivenPost && <PostsInfo posts={givenPosts}/>}
-                {isReceivedPost && <PostsInfo posts={receivedPosts}/>}
+                {isGivenPost && <PostsInfo posts={givenPosts} />}
+                {isReceivedPost && <PostsInfo posts={receivedPosts} />}
               </div>
-              <div className="col-lg-4 col-md-3 col-sm-12 col-xs-12">   
-                  { isGivenPost && givenPosts.length ? <PostList given={true} received={false} updatePosts={this.updatePosts} postsLoading={postsLoading} posts={givenPosts}/>: null}
-                  { isReceivedPost && receivedPosts.length ? <PostList received={true} given={false} updatePosts={this.updatePosts} postsLoading={postsLoading} posts={receivedPosts}/>: null}
-                  { isGivenPost && !givenPosts.length ? <Typography variant='h4' className="no-records">No posts Found</Typography>: null}
-                  { isReceivedPost && !receivedPosts.length ? <Typography variant='h4' className="no-records">No posts Found</Typography>: null}
+              <div className="col-lg-4 col-md-3 col-sm-12 col-xs-12">
+                {isGivenPost && givenPosts.length ? (
+                  <PostList
+                    given={true}
+                    received={false}
+                    updatePosts={this.updatePosts}
+                    postsLoading={postsLoading}
+                    posts={givenPosts}
+                  />
+                ) : null}
+                {isReceivedPost && receivedPosts.length ? (
+                  <PostList
+                    received={true}
+                    given={false}
+                    updatePosts={this.updatePosts}
+                    postsLoading={postsLoading}
+                    posts={receivedPosts}
+                  />
+                ) : null}
+                {isGivenPost && !givenPosts.length ? (
+                  <Typography variant="h4" className="no-records">
+                    No posts Found
+                  </Typography>
+                ) : null}
+                {isReceivedPost && !receivedPosts.length ? (
+                  <Typography variant="h4" className="no-records">
+                    No posts Found
+                  </Typography>
+                ) : null}
               </div>
               <div className="col-lg-0 col-md-4 col-sm-12 col-xs-12">
-                  { isReceivedPost && <RecentPosts posts={receivedPosts}/> }
-                  { isGivenPost && <RecentPosts posts={givenPosts}/> }
-                  { isReceivedPost && <TopPosts posts={receivedPosts}/> }
-                  { isGivenPost && <TopPosts posts={givenPosts}/> }
+                {isReceivedPost && <RecentPosts posts={receivedPosts} />}
+                {isGivenPost && <RecentPosts posts={givenPosts} />}
+                {isReceivedPost && <TopPosts posts={receivedPosts} />}
+                {isGivenPost && <TopPosts posts={givenPosts} />}
               </div>
-            </div>   
-        </div>)}
+            </div>
+          </div>
+        )}
       </React.Fragment>
-    );
+    )
   }
 }
 
 DashBoard.propTypes = {
   classes: PropTypes.object.isRequired,
-};
+}
 
 const mapStateToProps = state => {
-  const posts = state.getIn(['UserProfile', 'posts', 'success'], Map());
-  const postsLoading = state.getIn(['UserProfile', 'posts', 'loading'], Map());
+  const posts = state.getIn(['UserProfile', 'posts', 'success'], Map())
+  const postsLoading = state.getIn(['UserProfile', 'posts', 'loading'], Map())
   return {
     posts,
-    postsLoading
-  };
+    postsLoading,
+  }
 }
 
 const actionsToProps = {
   getPostsByUser: userProfileActions.getPostsByUser,
-  getPostsPostedByUser: userProfileActions.getPostsPostedByUser
+  getPostsPostedByUser: userProfileActions.getPostsPostedByUser,
 }
 
-export default withRouter(connect(mapStateToProps, actionsToProps)(DashBoard));
+export default withRouter(connect(mapStateToProps, actionsToProps)(DashBoard))
