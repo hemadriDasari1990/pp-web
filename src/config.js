c684f6d4bfe5dcda10e4bd73138a8822f1b75d1d
Hemadri Dssari <hemadri.dasari1990@gmail.com>
2020-01-09 17:01:35 +0530

    url update

--- src/config.js
+++ src/config.js
@@ -1,30 +1,29 @@
 const config = {
-	application: 'pp-web',
-	environment: global.NODE_ENV,
-	apiBaseUrl: 'http://'+window.location.href.split('/')[2],
-	API_URL: 'http://localhost:3000/api',
-	CLIENT_ROOT_URL:'http://localhost:8080',
-	URL_PREFIX: 'http://localhost:3000/api'
+  application: 'pp-web',
+  environment: global.NODE_ENV,
+  apiBaseUrl: 'http://' + window.location.href.split('/')[2],
+  API_URL: 'http://35.202.87.241:3000/api',
+  CLIENT_ROOT_URL: 'http://localhost:8080',
+  URL_PREFIX: 'http://35.202.87.241:3000/api',
 }
 
 switch (global.NODE_ENV) {
-	case 'production': {
-		const {protocol, hostname, port} = window.location
-		config.apiBaseUrl = `${protocol}//${hostname}${port ? `:${port}` : ''}`
-		
-		break;
-	}
-	case 'staging': {
-		
-		break;
-	}
-	case 'testing':
-		break;
-	default: {
-		config.URL_PREFIX = 'http://localhost:3000/api'
-		
-		break;
-	}
+  case 'production': {
+    const { protocol, hostname, port } = window.location
+    config.apiBaseUrl = `${protocol}//${hostname}${port ? `:${port}` : ''}`
+
+    break
+  }
+  case 'staging': {
+    break
+  }
+  case 'testing':
+    break
+  default: {
+    config.URL_PREFIX = 'http://35.202.87.241:3000/api'
+
+    break
+  }
 }
 
 export default config
