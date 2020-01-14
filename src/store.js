c7dc6bde38d47229227597430ef5edbd136d8094
Hemadri Dssari <hemadri.dasari1990@gmail.com>
2020-01-14 15:46:49 +0530

    store

--- src/store.js
+++ src/store.js
@@ -1,20 +1,20 @@
 import { createStore, compose, applyMiddleware } from 'redux'
-import {Map} from 'immutable';
+import { Map } from 'immutable'
 import thunk from 'redux-thunk'
 import throttle from 'lodash/throttle'
 import rootReducer from './reducers'
 // import { loginWithToken } from './actions'
 // import { saveState, loadState } from './helpers'
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export const store = createStore(
   rootReducer,
   Map(),
-  compose(
-  		applyMiddleware(thunk),
-  	 	window && window.devToolsExtension ? window.devToolsExtension() : f => f
-  	),
+  composeEnhancers(
+    applyMiddleware(thunk),
+    window && window.devToolsExtension ? window.devToolsExtension() : f => f,
+  ),
 )
 
 // store.subscribe(throttle(() => saveState(store.getState()), 1000))
