5c167046fac28cbf33595bf71d5101bd393f26bc
Hemadri Dssari <hemadri.dasari1990@gmail.com>
2020-03-07 10:25:31 +0530

    webpack changes for prod

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -4,84 +4,92 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const OfflinePlugin = require('offline-plugin')
 const PreloadWebpackPlugin = require('preload-webpack-plugin')
 
-module.exports = {
-  mode: 'production',
-  entry: {
-    main: resolve(__dirname, '../src'),
-    vendor: [
-      'react',
-      'react-dom',
-      'react-redux',
-      'react-router-dom',
-      'redux',
-      'redux-thunk',
-      'emotion',
+module.exports = (env, argv) => {
+  return {
+    entry: {
+      main: resolve(__dirname, '../src'),
+      vendor: [
+        'react',
+        'react-dom',
+        'react-redux',
+        'react-router-dom',
+        'redux',
+        'redux-thunk',
+        'emotion',
+      ],
+    },
+    output: {
+      filename: '[name].[chunkhash].js',
+      path: resolve(__dirname, '../dist'),
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          include: [resolve(__dirname, '../src')],
+          use: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader',
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|woff|woff2|svg)$/,
+          loader: 'url-loader?limit=100000',
+        },
+        {
+          test: /\.(eot|ttf)$/,
+          loader: 'file-loader',
+        },
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          loader: 'html-loader',
+        },
+        {
+          test: /\.scss$/,
+          loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production'),
+        },
+      }),
+      new webpack.optimize.UglifyJsPlugin(), //minify everything
+      new webpack.optimize.AggressiveMergingPlugin(), //Merge chunks
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor', 'manifest'],
+      }),
+      new HtmlWebpackPlugin({
+        hash: true,
+        filename: 'index.html',
+        title: 'writenpost',
+        template: 'webpack/template.html',
+        inject: false,
+      }),
+      new PreloadWebpackPlugin({
+        rel: 'preload',
+        as: 'script',
+        include: 'all',
+      }),
+      new OfflinePlugin({
+        ServiceWorker: {
+          navigateFallbackURL: '/',
+        },
+        AppCache: false,
+      }),
     ],
-  },
-  output: {
-    filename: '[name].[chunkhash].js',
-    path: resolve(__dirname, '../dist'),
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        include: [resolve(__dirname, '../src')],
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader',
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|woff|woff2|svg)$/,
-        loader: 'url-loader?limit=100000',
-      },
-      {
-        test: /\.(eot|ttf)$/,
-        loader: 'file-loader',
-      },
-      {
-        test: /\.html$/,
-        exclude: /node_modules/,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-    new webpack.optimize.UglifyJsPlugin(), //minify everything
-    new webpack.optimize.AggressiveMergingPlugin(), //Merge chunks
-    new webpack.optimize.CommonsChunkPlugin({
-      names: ['vendor', 'manifest'],
-    }),
-    new HtmlWebpackPlugin({
-      hash: true,
-      filename: 'index.html',
-      title: 'writenpost',
-      template: 'webpack/template.html',
-      inject: false,
-    }),
-    new PreloadWebpackPlugin({
-      rel: 'preload',
-      as: 'script',
-      include: 'all',
-    }),
-    new OfflinePlugin({
-      ServiceWorker: {
-        navigateFallbackURL: '/',
-      },
-      AppCache: false,
-    }),
-  ],
+    optimization: {
+      minimize: false,
+      minimizer:
+        argv.mode === 'production'
+          ? [new webpack.optimize.UglifyJsPlugin()]
+          : [],
+    },
+  }
 }
