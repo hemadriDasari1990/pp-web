7e3a5bbeb01c9dc0ef3ecb77f8c046cd16d39213
Hemadri Dssari <hemadri.dasari1990@gmail.com>
2020-03-07 21:35:34 +0530

    webpack improvements

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,5 +1,4 @@
 const { resolve } = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
