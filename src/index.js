0fe9764586b1c3dcca41b0ae1b0711ab77753185
Hemadri Dssari <hemadri.dasari1990@gmail.com>
2020-03-11 21:54:14 +0530

    corrections

--- src/index.js
+++ src/index.js
@@ -2,19 +2,20 @@ import Offline from 'offline-plugin/runtime'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { render } from 'react-dom'
-import { HashRouter, BrowserRouter, browserHistory } from 'react-router-dom'
+import { HashRouter, BrowserRouter } from 'react-router-dom'
 import { store } from './store'
 import '../assets/css/app.scss'
 import App from './components/App'
 import * as serviceWorker from './serviceworker'
+import { createBrowserHistory } from 'history'
 
 if (process.env.NODE_ENV === 'production') Offline.install()
 
 export const Root = () => (
   <Provider store={store}>
-    <HashRouter history={browserHistory}>
+    <BrowserRouter history={createBrowserHistory}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 )
 
