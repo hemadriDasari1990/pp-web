6452f54514907623fe6954ac2bef0307ed826577
Hemadri Dssari <hemadri.dasari1990@gmail.com>
2020-03-11 20:18:24 +0530

    revert to webpack3

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,21 +1,10 @@
 const { resolve } = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-// const OfflinePlugin = require('offline-plugin')
-// const PreloadWebpackPlugin = require('preload-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-// const imageminGifsicle = require('imagemin-gifsicle')
-// const imageminPngquant = require('imagemin-pngquant')
-// const imageminSvgo = require('imagemin-svgo')
-// const imageminMozjpeg = require('imagemin-mozjpeg')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const WebpackMd5Hash = require('webpack-md5-hash')
-const CompressionPlugin = require('compression-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+const OfflinePlugin = require('offline-plugin')
+const PreloadWebpackPlugin = require('preload-webpack-plugin')
 
 module.exports = {
-  devtool: '',
   entry: {
     main: resolve(__dirname, '../src'),
     vendor: [
@@ -33,7 +22,6 @@ module.exports = {
     path: resolve(__dirname, '../dist'),
     publicPath: '/',
   },
-  mode: 'production',
   module: {
     rules: [
       {
@@ -64,115 +52,34 @@ module.exports = {
       },
     ],
   },
-  stats: {
-    colors: false,
-    hash: true,
-    timings: true,
-    assets: true,
-    chunks: true,
-    chunkModules: true,
-    modules: true,
-    children: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
-    namedModules: false,
-    namedChunks: false,
-    nodeEnv: 'production',
-    flagIncludedChunks: true,
-    occurrenceOrder: true,
-    sideEffects: true,
-    usedExports: true,
-    concatenateModules: true,
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          enforce: true,
-          chunks: 'all',
-        },
-      },
-      minSize: 30000,
-      maxAsyncRequests: 5,
-      maxAsyncRequests: 3,
-      chunks: 'async',
-    },
-    noEmitOnErrors: true,
-    minimize: true,
-    minimizer: [
-      // we specify a custom UglifyJsPlugin here to get source maps in production
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        uglifyOptions: {
-          compress: false,
-          ecma: 6,
-          mangle: true,
-        },
-        sourceMap: true,
-      }),
-    ],
-    removeAvailableModules: true,
-    removeEmptyChunks: true,
-    mergeDuplicateChunks: true,
-  },
-  // optimization: {
-  //   minimizer: [
-  //     new UglifyJsPlugin({
-  //       sourceMap: true,
-  //       cache: true,
-  //       parallel: true,
-  //       // ecma: 5,
-  //       // warnings: false,
-  //       // ie8: false,
-  //       // safari10: false,
-  //       // dead_code: true,
-  //       // comments: false,
-  //       uglifyOptions: {
-  //         compress: {
-  //           inline: false,
-  //         },
-  //       },
-  //     }),
-  //     new OptimizeCSSAssetsPlugin({}),
-  //   ],
-  //   runtimeChunk: false,
-  //   splitChunks: {
-  //     cacheGroups: {
-  //       default: false,
-  //       commons: {
-  //         test: /[\\/]node_modules[\\/]/,
-  //         name: 'vendor_app',
-  //         chunks: 'all',
-  //         minChunks: 2,
-  //       },
-  //     },
-  //   },
-  // },
   plugins: [
-    new LodashModuleReplacementPlugin(),
     new webpack.optimize.ModuleConcatenationPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'style.[contenthash].css',
-    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
       },
     }),
+    new webpack.optimize.DedupePlugin(), //dedupe similar code
+    new webpack.optimize.UglifyJsPlugin(), //minify everything
     new webpack.optimize.AggressiveMergingPlugin(), //Merge chunks
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest'],
+    }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       title: 'writenpost',
       template: 'webpack/template.html',
     }),
-    new CompressionPlugin({
-      algorithm: 'gzip',
-      test: /\.js$|\.css$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
-      threshold: 10240,
-      minRatio: 0.8,
+    new PreloadWebpackPlugin({
+      rel: 'preload',
+      as: 'script',
+      include: 'all',
     }),
-    new WebpackMd5Hash(),
+    new OfflinePlugin({
+      ServiceWorker: {
+        navigateFallbackURL: '/',
+      },
+      AppCache: false,
+    }),
   ],
 }
